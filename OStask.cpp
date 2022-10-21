bba6c0bf34302ef53094e16986be34fa3ccb9f92
Gzh0821 <gaozih0821@outlook.com>
2022-10-21 16:12:33 +0800

    add copyright info.

--- OStask.cpp
+++ OStask.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2022 Gzh0821/Gaozih
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "OStask.h"
 
 namespace ost {
@@ -16,8 +36,8 @@ namespace ost {
                                                                   {PAGE_GUARD,             "Guard"},
                                                                   {PAGE_NOCACHE,           "No Cache"},
                                                                   {PAGE_WRITECOMBINE,      "Write Combine"},};
-    const std::unordered_map<DWORD, std::string> mbiTypeMap = {{MEM_IMAGE, "Image"},
-                                                               {MEM_MAPPED, "Mapped"},
+    const std::unordered_map<DWORD, std::string> mbiTypeMap = {{MEM_IMAGE,   "Image"},
+                                                               {MEM_MAPPED,  "Mapped"},
                                                                {MEM_PRIVATE, "Private"}};
 }
 
